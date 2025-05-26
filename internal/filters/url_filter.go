f763c32de36ac011dc70938821755c7d53d76ed2
Adit0507 <adi05072002@gmail.com>
2025-05-26 13:01:46 +0530

    url filtering

--- internal/filters/url_filter.go
+++ internal/filters/url_filter.go
@@ -1,7 +1,58 @@
 package filters
 
+import (
+	"net/url"
+	"strings"
+)
+
 type URLFilter struct {
 	blockedExtensions []string
-	blockedDomains []string
-	
+	blockedDomains    []string
+	allowedDomains    []string
+}
+
+func NewURLFilter(allowedDomains []string) *URLFilter {
+	return &URLFilter{
+		blockedExtensions: []string{".exe", ".zip", ".pdf", ".jpg", ".png", ".gif", ".mp4", ".avi", ".mov"},
+		blockedDomains:    []string{"facebook.com", "twitter.com", "instagram.com"},
+		allowedDomains:    allowedDomains,
+	}
+}
+
+func (f *URLFilter) ShouldCrawl(rawURL string) bool {
+	parsedURL, err := url.Parse(rawURL)
+	if err != nil {
+		return false
+	}
+
+	// checkin allowed domains for focused crawling
+	if len(f.allowedDomains) > 0 {
+		allowed := false
+		for _, domain := range f.allowedDomains {
+			if strings.Contains(parsedURL.Host, domain) {
+				allowed = true
+				break
+			}
+		}
+
+		if !allowed {
+			return false
+		}
+	}
+
+	// checkin blocked domains
+	for _, blocked := range f.blockedDomains {
+		if strings.Contains(parsedURL.Host, blocked) {
+			return false
+		}
+	}
+
+	path := parsedURL.Path
+	for _, ext := range f.blockedExtensions {
+		if strings.HasSuffix(strings.ToLower(path), ext) {
+			return false
+		}
+	}
+
+	return true
 }
